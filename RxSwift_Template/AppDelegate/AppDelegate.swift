bfee5035ed17b10504cb7fc37163bae20f0d1364
Phong Huynh N <phong.huynh@monstar-lab.com>
2023-02-09 14:29:26 +0700

    reset file AppDelegate

--- RxSwift_Template/AppDelegate/AppDelegate.swift
+++ RxSwift_Template/AppDelegate/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = HomeViewController()
+        let vc = ExampleViewController()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
         return true
