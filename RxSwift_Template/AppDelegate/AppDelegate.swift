7629e392e70fcf63b0799554d9e4b58cd4d76556
Luong Tran M. VN.Danang <>
2023-02-13 11:28:42 +0700

    Fix conflict Appdelegate

--- RxSwift_Template/AppDelegate/AppDelegate.swift
+++ RxSwift_Template/AppDelegate/AppDelegate.swift
@@ -29,12 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-<<<<<<< HEAD
-        let vc = HomeViewController()
-        window?.rootViewController = UINavigationController(rootViewController: vc)
-=======
         Session.shared.token.isEmpty ? (setRoot(rootType: .login)) : (setRoot(rootType: .home))
->>>>>>> main
         window?.makeKeyAndVisible()
         return true
     }
