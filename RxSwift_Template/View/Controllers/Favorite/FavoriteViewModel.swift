077c54469fc7a3a89f3b9887893a59cfb4183a6b
Phong Huynh N <phong.huynh@monstar-lab.com>
2023-02-16 16:19:20 +0700

    Revert "Fix cmt"
    This reverts commit f50a50264650e6d5ac1e374d0eba6c29e203c2f5.

--- RxSwift_Template/View/Controllers/Favorite/FavoriteViewModel.swift
+++ RxSwift_Template/View/Controllers/Favorite/FavoriteViewModel.swift
@@ -12,10 +12,9 @@ import RxCocoa
 final class FavoriteViewModel {
     
     private var bag: DisposeBag = DisposeBag()
-    var favorites: BehaviorRelay<[Product]> = .init(value: [Product(name: "Favorite test", imageProduct: "https://cdn.tgdd.vn/Files/2019/07/25/1181734/do-sau-truong-anh-la-gi-cach-thiet-lap-de-chup-anh-dep-nhat--1.jpg", category: Category(shop: Shop(nameShop: "Shop test")))])
-    var errorBehaviorRelay: PublishRelay<Error> = .init()
+    var favoriteProducts: BehaviorRelay<[Product]> = .init(value: [Product(name: "Favorite test", imageProduct: "https://cdn.tgdd.vn/Files/2019/07/25/1181734/do-sau-truong-anh-la-gi-cach-thiet-lap-de-chup-anh-dep-nhat--1.jpg", category: Category(shop: Shop(nameShop: "Shop test")))])
     
     func viewModelForItem(index: Int) -> FavoriteTableCellViewModel {
-        return FavoriteTableCellViewModel(product: favorites.value[index])
+        return FavoriteTableCellViewModel(favoriteProduct: favoriteProducts.value[index])
     }
 }
