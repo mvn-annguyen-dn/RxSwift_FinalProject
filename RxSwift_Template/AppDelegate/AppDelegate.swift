b6fdaec1853cd30f1c661c2634a5e3c3f15e832b
Luong Tran M. VN.Danang <>
2023-02-23 17:27:29 +0700

    Refactor code

--- RxSwift_Template/AppDelegate/AppDelegate.swift
+++ RxSwift_Template/AppDelegate/AppDelegate.swift
@@ -29,8 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-//        Session.shared.token.isEmpty ? (setRoot(rootType: .login)) : (setRoot(rootType: .home))
-        (setRoot(rootType: .home))
+        Session.shared.token.isEmpty ? (setRoot(rootType: .login)) : (setRoot(rootType: .home))
         window?.makeKeyAndVisible()
         return true
     }
