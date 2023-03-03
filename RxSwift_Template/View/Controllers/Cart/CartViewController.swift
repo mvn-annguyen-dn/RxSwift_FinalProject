b046d4133e1ec47547a4555a6782e88e207051df
Phong Huynh N <phong.huynh@monstar-lab.com>
2023-03-03 13:39:18 +0700

    fix cmt

--- RxSwift_Template/View/Controllers/Cart/CartViewController.swift
+++ RxSwift_Template/View/Controllers/Cart/CartViewController.swift
@@ -33,7 +33,7 @@ final class CartViewController: BaseViewController {
         configDatasourcce()
         configUI()
         getCart()
-        setupUI()
+        bindingViewModel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -58,69 +58,68 @@ final class CartViewController: BaseViewController {
     }
     
     private func configDatasourcce() {
-        viewModel.carts.bind(to: tableView.rx.items(cellIdentifier: Define.cellName, cellType: CartTableViewCell.self)) { [weak self] index, element, cell in
-            guard let this = self else { return }
-            cell.viewModel = this.viewModel.viewModelForItem(index: index)
-            
-            cell.rx
-                .increase
-                .subscribe(onNext: {
-                    this.updateCart(orderId: element.id ?? 0, quantity: (element.quantity ?? 0) + 1)
-                    this.updatePriceInfoView()
-                })
-                .disposed(by: cell.bag)
-            
-            cell.rx
-                .decrease
-                .subscribe(onNext: {
-                    let numberItemCart = (element.quantity ?? 0) - 1
-                    if numberItemCart != 0 {
+        viewModel.carts
+            .bind(to: tableView.rx.items(cellIdentifier: Define.cellName, cellType: CartTableViewCell.self)) { [weak self] index, element, cell in
+                guard let this = self else { return }
+                cell.viewModel = this.viewModel.viewModelForItem(cart: element)
+                
+                cell.rx
+                    .increase
+                    .subscribe(onNext: {
+                        let numberItemCart = (element.quantity ?? 0) + 1
                         this.updateCart(orderId: element.id ?? 0, quantity: numberItemCart)
-                    } else {
-                        var deleteCart = this.viewModel.carts.value
-                        deleteCart.remove(at: index)
-                        this.viewModel.carts.accept(deleteCart)
-                        this.updateCart(orderId: element.id ?? 0, quantity: numberItemCart)
-                    }
-                    this.updatePriceInfoView()
-                })
-                .disposed(by: cell.bag)
-            
-            cell.rx
-                .inputQuantity
-                .subscribe(onNext: { quantity in
-                    if quantity.isNumeric {
-                        this.updateCart(orderId: element.id ?? 0, quantity: Int(quantity) ?? 0)
-                    } else {
-                        this.normalAlert(message: "Format invalid")
-                    }
-                    this.updatePriceInfoView()
-                })
-                .disposed(by: cell.bag)
-        }
-        .disposed(by: bag)
-    }
-    
-    private func updateUI() {
-        emptyView.rx.isHidden.onNext(viewModel.carts.value.isEmpty ? false : true)
+                    })
+                    .disposed(by: cell.bag)
+                
+                cell.rx
+                    .decrease
+                    .subscribe(onNext: {
+                        let numberItemCart = (element.quantity ?? 0) - 1
+                        if numberItemCart != 0 {
+                            this.updateCart(orderId: element.id ?? 0, quantity: numberItemCart)
+                        } else {
+                            this.removeCart()
+                            this.updateCart(orderId: element.id ?? 0, quantity: numberItemCart)
+                        }
+                    })
+                    .disposed(by: cell.bag)
+                
+                cell.rx
+                    .inputQuantity
+                    .subscribe(onNext: { quantity in
+                        if quantity.isNumeric {
+                            this.updateCart(orderId: element.id ?? 0, quantity: Int(quantity) ?? 0)
+                        } else {
+                            this.normalAlert(message: "Format invalid")
+                        }
+                    })
+                    .disposed(by: cell.bag)
+            }
+            .disposed(by: bag)
     }
     
     private func updatePriceInfoView() {
         let formatter = NumberFormatter()
         formatter.rx.locale.onNext(Locale.current)
         formatter.rx.numberStyle.onNext(.currency)
-        let totalPrice = viewModel.totalPriceCarts()
         
         viewModel.carts
             .subscribe(onNext: { cart in
                 let selectedItem = "Item selected: \(cart.count)"
-                self.selectedItemLabel.rx.text.onNext(selectedItem)
+                self.selectedItemLabel.rx
+                    .text
+                    .onNext(selectedItem)
+                
+                let total = cart.reduce(0) {retVal, value in
+                    retVal + ((value.quantity ?? 0) * (value.price ?? 0))
+                }
+                if let formattedTipAmount = formatter.string(from: total as NSNumber) {
+                    self.totalPriceLabel.rx
+                        .text
+                        .onNext("\(formattedTipAmount)")
+                }
             })
             .disposed(by: bag)
-        
-        if let formattedTipAmount = formatter.string(from: totalPrice.value as NSNumber) {
-            totalPriceLabel.rx.text.onNext("\(formattedTipAmount)")
-        }
     }
     
     private func animationLoadTable() {
@@ -131,34 +130,83 @@ final class CartViewController: BaseViewController {
     }
     
     private func configUI() {
-        priceInfoView.layer.rx.cornerRadius.onNext(Define.cornerRadius)
-        priceInfoView.layer.rx.maskedCorners.onNext(Define.maskedCorners)
-        priceInfoView.layer.rx.shadowColor.onNext(Define.shadowColor)
-        priceInfoView.layer.rx.shadowOpacity.onNext(Define.shadowOpacity)
-        priceInfoView.layer.rx.shadowOffset.onNext(Define.shadowOffset)
-        priceInfoView.layer.rx.shadowRadius.onNext(Define.shadowRadius)
-        priceInfoView.layer.rx.masksToBounds.onNext(false)
-        checkOutButton.layer.rx.cornerRadius.onNext(Define.cornerRadius)
+        priceInfoView.layer
+            .rx
+            .cornerRadius
+            .onNext(Define.cornerRadius)
+        priceInfoView.layer
+            .rx
+            .maskedCorners
+            .onNext(Define.maskedCorners)
+        priceInfoView.layer
+            .rx
+            .shadowColor
+            .onNext(Define.shadowColor)
+        priceInfoView.layer
+            .rx
+            .shadowOpacity
+            .onNext(Define.shadowOpacity)
+        priceInfoView.layer
+            .rx
+            .shadowOffset
+            .onNext(Define.shadowOffset)
+        priceInfoView.layer
+            .rx
+            .shadowRadius
+            .onNext(Define.shadowRadius)
+        priceInfoView.layer
+            .rx
+            .masksToBounds
+            .onNext(false)
+        checkOutButton.layer
+            .rx
+            .cornerRadius
+            .onNext(Define.cornerRadius)
         
         let tapView = UITapGestureRecognizer()
         tapView.addTarget(self, action: #selector(showHideViewDetail))
         priceInfoView.addGestureRecognizer(tapView)
     }
-    
-    private func setupUI() {
-        viewModel.loadingCart.distinctUntilChanged().subscribe(onNext: { [weak self] isLoadingCart in
-            guard let this = self else { return }
-            if isLoadingCart {
-                this.updateUI()
-                this.updatePriceInfoView()
-                this.animationLoadTable()
-            }
-        })
-        .disposed(by: bag)
+    func bindingViewModel() {
+        viewModel.carts
+            .map { !$0.isEmpty }
+            .bind(to: emptyView.rx.isHidden)
+            .disposed(by: bag)
+        
+        viewModel.carts
+            .subscribe(onNext: { _ in
+                self.animationLoadTable()
+            })
+            .disposed(by: bag)
+        
+        updatePriceInfoView()
     }
     
     private func getCart() {
         viewModel.getApiCart()
+    }
+    
+    private func removeCart() {
+        guard let indexPath = tableView.indexPath(for: CartTableViewCell())
+        else {
+            return
+        }
+        var removeCart: [Cart] = []
+        viewModel.carts.subscribe(onNext: { carts in
+            removeCart = carts
+            removeCart.remove(at: indexPath.row)
+        })
+        .disposed(by: bag)
+    }
+    
+    private func removeAllCart() {
+        var removeAllCart: [Cart] = []
+        viewModel.carts.subscribe(onNext: { carts in
+            print("phong remove All Cart")
+            removeAllCart = carts
+            removeAllCart.removeAll()
+        })
+        .disposed(by: bag)
     }
     
     // MARK: - Objc methods
@@ -181,10 +229,7 @@ final class CartViewController: BaseViewController {
     @objc private func deleteButtonTouchUpInside() {
         let refreshAlert = UIAlertController(title: "Delete", message: "Do you wanna delete all products in cart?", preferredStyle: UIAlertController.Style.alert)
         refreshAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
-            var deleteCart = self.viewModel.carts.value
-            deleteCart.removeAll()
-            self.viewModel.carts.accept(deleteCart)
-            self.animationLoadTable()
+            self.removeAllCart()
         }))
         refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(refreshAlert, animated: true, completion: nil)
@@ -264,10 +309,13 @@ extension CartViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let delete = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, completionHandler) in
             guard let this = self else { return }
-            this.deleteCart(orderId: this.viewModel.carts.value[indexPath.row].id ?? 0)
+            this.viewModel.carts.subscribe(onNext: { carts in
+                this.deleteCart(orderId: carts[indexPath.row].id ?? 0)
+            })
+            .disposed(by: this.bag)
             completionHandler(true)
         }
         delete.backgroundColor = .white
-        return UISwipeActionsConfiguration(actions: [delete])
+        return UISwipeActionsConfiguration(actions: [])
     }
 }
--- RxSwift_Template/View/Controllers/Cart/CartViewModel.swift
+++ RxSwift_Template/View/Controllers/Cart/CartViewModel.swift
@@ -11,9 +11,8 @@ import RxCocoa
 final class CartViewModel {
     
     private var bag: DisposeBag = DisposeBag()
-    var carts: BehaviorRelay<[Cart]> = .init(value: [])
+    var carts: PublishRelay<[Cart]> = .init()
     var errorBehaviorRelay: PublishRelay<ApiError> = .init()
-    var loadingCart: BehaviorRelay<Bool> = .init(value: false)
     
     func getApiCart() {
         ApiNetWorkManager.shared.request(CartResponse.self, .target(MainTarget.cart))
@@ -21,10 +20,8 @@ final class CartViewModel {
                 switch response {
                 case .success(let carts):
                     self.carts.accept(carts.data ?? [])
-                    self.loadingCart.accept(true)
                 case .failure(let error):
                     self.errorBehaviorRelay.accept(error as? ApiError ?? .invalidResponse)
-                    self.loadingCart.accept(false)
                 }
             }
             .disposed(by: bag)
@@ -35,18 +32,10 @@ final class CartViewModel {
     }
     
     func requestDeleteCart(orderId: Int) -> Single<MessageResponse> {
-       return ApiNetWorkManager.shared.request(MessageResponse.self, .target(MainTarget.deleteCart(id: orderId)))
+        return ApiNetWorkManager.shared.request(MessageResponse.self, .target(MainTarget.deleteCart(id: orderId)))
     }
     
-    func viewModelForItem(index: Int) -> CartCellViewModel {
-        return CartCellViewModel(cart: carts.value[safe: index])
-    }
-    
-    func totalPriceCarts() -> BehaviorRelay<Int> {
-        let total: BehaviorRelay<Int> = .init(value: 0)
-        for cart in carts.value {
-            total.accept(total.value + (cart.quantity ?? 0) * (cart.price ?? 0))
-        }
-        return total
+    func viewModelForItem(cart: Cart) -> CartCellViewModel {
+        return CartCellViewModel(cart: cart)
     }
 }
